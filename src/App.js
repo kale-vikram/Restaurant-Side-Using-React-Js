7eaacac0183a1312d30b5c9eaf2ed75d9fb7368e
kale-vikram <88614613+kale-vikram@users.noreply.github.com>
2023-01-19 10:23:22 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Restaurant from "./Component/Restaurant/Restaurant";
+
+const App = () => {
+  return (
+    <>
+      <Restaurant />
+    </>
+  );
+};
+
+export default App;
